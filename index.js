e658cf16b5b9000eca7426225f4b2d8aa000218f
ranieriprg <eddieranieri.prg@gmail.com>
2024-06-16 00:40:26 -0300

    post para cadastro de livros

--- index.js
+++ index.js
@@ -4,14 +4,38 @@ const { engine } = require("express-handlebars");
 const mysql = require("mysql");
 
 const app = express();
-
-app.engine("handlebars", engine());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+app.engine(
+  "handlebars",
+  engine({
+    defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
 app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("hello teste");
+  res.render("home");
+});
+
+app.post("/books/insertbook", (req, res, next) => {
+  const title = req.body.title
+  const pagesqty = req.body.pagesqty;
+  console.log(`O titulo do livro é ${title} e ele possui ${pagesqty}`);
+const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pagesqty}')`;
+conn.query(sql, function(err) {
+  if(err){
+    console.log('error', err);
+  }
+  res.redirect('/')
+})
 });
 
 const conn = mysql.createConnection({
@@ -23,7 +47,7 @@ const conn = mysql.createConnection({
 
 conn.connect(function (err) {
   if (err) {
-    console.log('erro ao conectar com o banco',err);
+    console.log("erro ao conectar com o banco", err);
   }
   console.log("banco de dados conectado");
 });
