9bf9c6e461330894050d85e2975f2cc9dc9c7147
ranieriprg <eddieranieri.prg@gmail.com>
2024-06-16 21:26:38 -0300

    post remove done

--- index.js
+++ index.js
@@ -28,28 +28,78 @@ app.get("/", (req, res) => {
 app.post("/books/insertbook", (req, res) => {
   const title = req.body.title;
   const pagesqty = req.body.pagesqty;
-  console.log(`O titulo do livro é ${title} e ele possui ${pagesqty}`);
   const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pagesqty}')`;
   conn.query(sql, function (err) {
     if (err) {
       console.log("error", err);
-      return
+      return;
     }
     res.redirect("/");
   });
 });
 
 app.get("/books", (req, res) => {
-  const sql = "SELECT * FROM books"
-  conn.query(sql, function(err, data) {
-    if(err){
-      console.log('falha ao buscar books');
-      return
+  const sql = "SELECT * FROM books";
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log("falha ao buscar books");
+      return;
     }
-    const books = data
+    const books = data;
     console.log(books);
-    res.render('books', {books})
-  })
+    res.render("books", { books });
+  });
+});
+
+app.get("/books/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM books WHERE id = ${id}`;
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log("Erro ao buscar o livro por id");
+    }
+    const book = data[0];
+    res.render("book", { book });
+  });
+});
+
+app.get("/books/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM books WHERE id = ${id}`;
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const book = data[0];
+    res.render("editbook", { book });
+  });
+});
+
+app.post("/books/updatebook", (req, res) => {
+  const id = req.body.id;
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
+
+  const sql = `UPDATE books SET title = '${title}', pageqty = '${pageqty}' WHERE id = ${id}`;
+  conn.query(sql, function (err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.redirect("/books");
+  });
+});
+
+app.post("/books/remove/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = `DELETE FROM books WHERE id = ${id}`;
+  conn.query(sql, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/books");
+  });
 });
 
 const conn = mysql.createConnection({
